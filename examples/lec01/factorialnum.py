6271669a0281996bb8773fa4ea15ff4c9aa77109
aurorarealm <jianlv126@gmail.com>
2025-02-23 03:51:15 +0800

    把‘一’改成‘1’

--- examples/lec01/factorialnum.py
+++ examples/lec01/factorialnum.py
@@ -3,5 +3,5 @@ def factorialnum(n):
            return 1
       return n * factorialnum(n - 1)
 # 测试一下
-num = int(input("输入一个非负整数："))
+num = int(input("输入1个非负整数："))
 print(f" {num} 的阶乘是 {factorialnum(num)}")
