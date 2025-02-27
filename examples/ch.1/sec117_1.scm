3e11e804269cb0182c9766cdae43daf4fd6bb39d
aurorarealm <jianlv126@gmail.com>
2025-02-27 15:53:54 +0800

    原始函数

--- examples/ch.1/sec117_1.scm
+++ examples/ch.1/sec117_1.scm
@@ -1,35 +1,22 @@
 #lang scheme
+(define (square x) (* x x))
+;(square 2)
+(define (good-enough? guess x)
+        (< (abs (- (square guess) x)) 0.001)
+  )
 
-;1 主函数：涉及3个函数，自身、good-enough?、improve
 (define (sqrt-iter guess x)
         (if (good-enough? guess x)
             guess
             (sqrt-iter (improve guess x) x)
          )   
   )
-;定义函数sqrt-iter，参数为guess和x。 如果“good-enough?”为true，则为guess；否则再求函数
-;sqrt-iter，但是参数guess调整为improve，参数为guess和x。
-;------------------------
 
-;1.1 good-enough?函数，涉及1个square求平方函数
-(define (good-enough? guess x)
-        (< (abs (- (square guess) x)) 0.001)
-  );定义函数"good-enough?",参数为guess和x。函数的意思是 |(guess^2)-x| 是否小于0.001
-;----------
-;1.1.1 square函数
-(define (square x) (* x x))
-;(square 2)
-;---------------
-
-;1.2 improve函数，参数是guess x
 (define (improve guess x)
         (average guess (/ x guess))
-  );improve函数，参数是guess、x/guess。
-;1.2.1 improve函数涉及有一个average函数
-(define (average x y) (/ (+ x y) 2));定义average函数，参数x,y。函数体是：（x+y）/2
-;-----------------
+  )
+(define (average x y) (/ (+ x y) 2))
 
-;求解：
-(sqrt-iter 0.0000001 2)
+(sqrt-iter 1.0 2)
 
 ;answer：1.4144272852751953
