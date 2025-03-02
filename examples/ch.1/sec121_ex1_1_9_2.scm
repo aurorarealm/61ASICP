847c97b3507dcd3299b9fb7ea2bbd3fedab4bcc6
aurorarealm <jianlv126@gmail.com>
2025-03-02 12:21:14 +0800

    两个数相加，迭代，有trace

---
+++ examples/ch.1/sec121_ex1_1_9_2.scm
@@ -0,0 +1,32 @@
+#lang scheme
+;Each of the following two procedures defines a method for adding two positive integers in terms of the procedures inc, which increments its argument by 1 , 
+(require scheme/trace)  ; 导入 trace 模块
+
+  (define (inc x) (+ x 1))
+  (define (dec x) (- x 1))
+(define (plus a b)
+
+  (if (= a 0)
+      b
+      (plus (dec a) (inc b))
+       )
+    )
+(trace plus)  ; 追踪   函数
+(plus 3 2)
+
+;>(plus 3 2)
+;>(plus 2 3)
+;>(plus 1 4)
+;>(plus 0 5)
+;<5
+;5
+
+
+
+
+
+
+
+
+
+
